c00cea9e5daba4f6ae92515107594c6293931ae2
joyce chi <joycechi512@gmail.com>
2020-07-14 20:12:23 -0700

    Initial Commit

---
+++ atlasApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  atlasApp
+//
+//  Created by Joyce on 7/14/20.
+//  Copyright © 2020 Joyce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
