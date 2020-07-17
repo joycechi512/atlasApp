8be9fd5e8f5134d864a1c7a413231e4a506357bd
Jennie Pham <jenniepham14@gmail.com>
2020-07-16 19:42:01 -0700

    Iteration with App Icon, Fixed Files, Storyboard

---
+++ atlasApp/IceBillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IceBillViewController.swift
+//  atlasApp
+//
+//  Created by Jennie Pham on 7/16/20.
+//  Copyright © 2020 Joyce. All rights reserved.
+//
+
+import UIKit
+
+class IceBillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func IceBilltoInfoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func IceBilltoIndexTapped(_ sender: Any) {
+    }
+    
+   
+    @IBAction func IceBillToHomeTapped(_ sender: Any) {
+    }
+
+    
+}
